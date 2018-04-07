7c8840671d65baf87015684c1498530e8d048c6b
ZafarAqil <Zafaraqil14@gmail.com>
2018-04-07 17:24:05 +0530

    bugs and styling is done

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,9 +4,10 @@ class Form extends React.Component {
     render() {
         return (
             <form onSubmit={this.props.getWeather}>
-                <input type="text" name="city" placeholder="City..."/>
-                <input type="text" name="country" placeholder="Country..."/>
-                <button>Get Weather </button>
+                <input type="text" name="city" placeholder="City"/>
+                <input type="text" name="country" placeholder="Country"/>
+                <button>Get Weather</button>
+                
             </form>
         );
     }
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,7 +4,7 @@ class Titles extends React.Component {
     render() {
         return(
             <div>
-                <h1 className="title-container__title">Weather Finder!</h1>
+                <h1 className="title-container__title">Weather Finder</h1>
                 <h3 className="title-container__subtitle"> Find out temperature, conditions and more...</h3>
             </div>
         );
